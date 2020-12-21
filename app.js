445f725fd9a91b5fc94ff6df68869354df7c8951
Ruth Ann Hoatson <74863790+Ruth-Ann-Hoatson@users.noreply.github.com>
2020-12-21 16:43:49 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const button =document.querySelector('#btn');
+
+button.addEventListener('click', function() {
+    const append = document.querySelector ('.booking_block').innerHTML=" Booking has been made.See you soon!"
+})
+
